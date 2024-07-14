d45332eb3b4cc1948f5c4287e8ca98df4f981d03
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-14 07:37:08 +0500

    736am 14 07 2024

---
+++ online.ts
@@ -0,0 +1,32 @@
+// functions CallBacks
+
+// expect number
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+// expect string
+function great(name: string): string {
+    return `Hello ${name}`;
+}
+
+// expect Boolen 
+function powerOutage(Light: boolean): void  {
+    if(Light) {
+        console.log("Light is on");
+    } else {
+        console.log("Light is off");
+    }
+}
+
+// expect array 
+function customfind(arr: string[], element: string): boolean | void {
+    for(let i = 0; i < arr.length; i++){
+        if()
+    }
+
+}
+
+
+
+
