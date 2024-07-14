bf1ab47c33391c698037fec2ae3b2ee582570c5d
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-14 16:16:51 +0500

    Update washing.ts

--- washing.ts
+++ washing.ts
@@ -10,3 +10,27 @@ function washing(callback){
 }
 
 
+function soaking(callback){
+    console.log("soaking started");
+    setTimeout(() => {
+        console.log("soaking done");
+        callback();
+    }, 5000);
+}
+
+// function ironing(callback){
+//     console.log("ironing started"); 
+//     setTimeout(() => {
+//         console.log("ironing done");
+//         callback();
+//     }, 3000);
+// }
+
+function drying(callback){
+    console.log("drying started");
+    setTimeout(() => {
+        console.log("drying done");
+        callback();
+    }, 3000);
+}
+
