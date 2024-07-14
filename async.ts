9c48e9f5b162734a0236d1e5faaa535588963256
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-14 15:56:09 +0500

    Update async.ts

--- async.ts
+++ async.ts
@@ -5,3 +5,12 @@ setTimeout(function() {
     console.log('Hello World');
 }, 2000); // 2 seconds delay, we write as 2000miliseconds
 
+function greet() {
+    console.log('Hello mera World');
+}
+
+setTimeout(greet, 2000);
+
+
+
+
