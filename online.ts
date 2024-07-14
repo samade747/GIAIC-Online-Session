7142db4d8e5c5d716a12964e942139ccbdc78e6a
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-14 08:58:54 +0500

    858

--- online.ts
+++ online.ts
@@ -1,16 +1,24 @@
 // functions CallBacks
 
+
+
 // expect number
+// data type is number
+// primitive data type
 function add(a: number, b: number): number {
     return a + b;
 }
 
 // expect string
+// data type is string
+// primitive data type
 function great(name: string): string {
     return `Hello ${name}`;
 }
 
 // expect Boolen 
+// data type is boolean
+// primitive data type
 function powerOutage(Light: boolean): void  {
     if(Light) {
         console.log("Light is on");
@@ -20,12 +28,35 @@ function powerOutage(Light: boolean): void  {
 }
 
 // expect array 
+// data type is array
+// 
 function customfind(arr: string[], element: string): boolean | void {
     for(let i = 0; i < arr.length; i++){
-        if()
+        if(arr[i] === element){
+            return true;}
     }
 
 }
+
+// expect Object 
+// data type is Object
+// 
+function logPersonName(person: TPerson): void {
+    console.log(person.name);
+}
+
+
+type TPerson = {
+    name: string;
+    age: number;
+}
+
+const person: TPerson = {
+    name: "John",
+    age: 30
+}
+
+console.log(logPersonName(person));
 
 
 
---
+++ slidescallback.ts
@@ -0,0 +1,21 @@
+// // CallBacks with function
+// function processUserInput(callback: any){
+//     let linkedin = " samaddeveloper";
+
+//     callback(linkedin);
+
+// }
+
+// // expect string 
+// // data type is string
+// processUserInput(function(linkedin:string){
+//     console.log(`my linkedin username is ${linkedin}`)
+// })
+
+
+
+
+
+
+
+// 2 example
