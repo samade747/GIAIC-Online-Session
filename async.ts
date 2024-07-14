6ada93eef77eef2e2c1f3601d9e6346d5b1e0e00
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-14 15:20:26 +0500

    0

---
+++ async.ts
@@ -0,0 +1,7 @@
+// async function
+// setTimeout
+
+setTimeout(function() {
+    console.log('Hello World');
+}, 2000); // 2 seconds delay, we write as 2000miliseconds
+
--- sync.ts
+++ sync.ts
@@ -54,4 +54,6 @@ console.log('Ending...');
 
 
 
+// javascript run line by line in synchronous by default
 
+// javascript JavaScript is an asynchronous and concurrent programming language that offers a lot of flexibility. It's single-threaded like synchronous but also non-blocking like asynchronous. Although it's synchronous by nature, JavaScript benefits from an asynchronous process
