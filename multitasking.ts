ce32db09b736c774abd36b15bc57e3b4bd3258fc
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-16 17:49:25 +0500

    0

---
+++ multitasking.ts
@@ -0,0 +1,54 @@
+// call back
+// promises
+// async await
+
+
+// behind the scene in backend 
+// event lopp + node apis 
+
+
+// function washing(callback){
+//     console.log("washing started"); 
+//     setTimeout(() => {
+//         console.log("washing done");
+//         callback();
+//     }, 5000);
+
+// }
+
+
+function washing(callback){
+
+    console.log("washing started");
+
+    setTimeout(() => {
+        console.log("washing done");
+        callback();
+    }, 5000);
+
+}
+
+
+function soaking(callback){
+    console.log("soaking started");
+    setTimeout(() => {
+        console.log("soaking done");
+        callback();
+    }, 5000);
+}
+
+// function ironing(callback){
+//     console.log("ironing started"); 
+//     setTimeout(() => {
+//         console.log("ironing done");
+//         callback();
+//     }, 3000);
+// }
+
+function drying(callback){
+    console.log("drying started");
+    setTimeout(() => {
+        console.log("drying done");
+    }, 3000);
+}
+
--- washing.ts
+++ washing.ts
@@ -1,36 +1,36 @@
-function washing(callback){
+// function washing(callback){
 
-    console.log("washing started");
+//     console.log("washing started");
 
-    setTimeout(() => {
-        console.log("washing done");
-        callback();
-    }, 5000);
-
-}
-
-
-function soaking(callback){
-    console.log("soaking started");
-    setTimeout(() => {
-        console.log("soaking done");
-        callback();
-    }, 5000);
-}
-
-// function ironing(callback){
-//     console.log("ironing started"); 
 //     setTimeout(() => {
-//         console.log("ironing done");
+//         console.log("washing done");
+//         callback();
+//     }, 5000);
+
+// }
+
+
+// function soaking(callback){
+//     console.log("soaking started");
+//     setTimeout(() => {
+//         console.log("soaking done");
+//         callback();
+//     }, 5000);
+// }
+
+// // function ironing(callback){
+// //     console.log("ironing started"); 
+// //     setTimeout(() => {
+// //         console.log("ironing done");
+// //         callback();
+// //     }, 3000);
+// // }
+
+// function drying(callback){
+//     console.log("drying started");
+//     setTimeout(() => {
+//         console.log("drying done");
 //         callback();
 //     }, 3000);
 // }
-
-function drying(callback){
-    console.log("drying started");
-    setTimeout(() => {
-        console.log("drying done");
-        callback();
-    }, 3000);
-}
 
